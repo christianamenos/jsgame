cd2de90a4ec606261080fbd4a04a6d5e8d8d8b4a
Christian <christian.amenos@gmail.com>
2021-08-17 20:38:17 +0200

    Refactor to draw the player and coordinates

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
 const canvas = document.getElementById('viewport');
 const context = canvas.getContext('2d');
 
-const x = 300;
-const y = 150;
-const width = 25;
-const height = 50;
+class Coord {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
 
-context.beginPath();
-context.rect(x, y, width, height);
-context.fillStyle = '#ff0000'; // RGB R G B - 0 - f 16 * 16  = 256
-context.fill();
-context.closePath();
+class Player {
+    constructor(coordinates) {
+        this.position = coordinates;
+        this.width = 25;
+        this.height = 50;
+        this.color = '#ff0000';
+    }
+
+    draw(context) {
+        context.beginPath();
+        context.rect(this.position.x, this.position.y, this.width, this.height);
+        context.fillStyle = this.color; // RGB R G B - 0 - f 16 * 16  = 256
+        context.fill();
+        context.closePath();
+    }
+}
+
+const playerCoord = new Coord(300, 150);
+const player = new Player(playerCoord);
+player.draw(context);
+
+
 
 
